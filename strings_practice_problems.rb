747dbc086b463cbb085c36414dbacfe593e24da8
Yitzy Bratt <yitzybratt32@gmail.com>
2021-02-21 23:18:23 -0500

    completes ruby_strings_practice

--- strings_practice_problems.rb
+++ strings_practice_problems.rb
@@ -1,19 +1,84 @@
 # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
+# p "Please enter your first name"
+# first_name = gets.chomp
+# p "Please enter your last name"
+# last_name = gets.chomp
+# p first_name + " " + last_name
+
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+# p "Please enter your first name"
+# first_name = gets.chomp
+# p "Please enter your last name"
+# last_name = gets.chomp
+
+# p "#{first_name} #{last_name}"
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
+# p "Please enter a word"
+# word = gets.chomp
+# if word == "marco"
+#   p "polo"
+# end
+
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+# color1 = "blue"
+# color2 = "green"
+# color3 = "purple"
+
+# p "On a beautiful day, with a" + " " + color1 + " " + "sky, and" + " " + color2 + " " + "grass,some" + " " + color3 + " " + "hippos rested outside"
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
+# color1 = "blue"
+# color2 = "green"
+# color3 = "purple"
+
+# p "On a beautiful day, with a #{color1} sky, #{color2} grass, some #{color3} hippos rested in the grass"
+
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+# p "Please enter your name"
+# input = gets.chomp
+# if input != "santa"
+#   p "You're not Santa"
+# end
 
 # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
 
+# title1 = "The very hungry caterpillar"
+# author1 = "Eric Carl"
+# title2 = "The Cat in the Hat"
+# author2 = "Dr. Seuss"
+
+# p "we read" + " " + title1 + " " + "by" + " " + author1 + " " + "and" + " " + title2 + " " + "by" + " " + author2 + "."
+
 # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+# title1 = "The very hungry caterpillar"
+# author1 = "Eric Carl"
+# title2 = "The Cat in the Hat"
+# author2 = "Dr. Seuss"
+
+# p "We read #{title1} by #{author1} and #{title2} by #{author2}"
 
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
+# p "Please enter a password"
+# input = gets.chomp
+# if input == "Joshua"
+#   p "Shall we play a game?"
+# else
+#   p "Access denied"
+# end
+
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+# city1 = "NY"
+# city2 = "NJ"
+# city3 = "CA"
+
+# p "first we went to" + " " + city1 + " " + "then to" + " " + city2 + " " + "then to" + " " + city3 + "."
