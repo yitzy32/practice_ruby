40bdb7a50e24db850cede2c9d50883b4b1ee86df
Yitzy Bratt <yitzybratt32@gmail.com>
2021-02-21 21:24:37 -0500

    complete loops. init string_practice

--- loops.rb
+++ loops.rb
@@ -27,12 +27,54 @@
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
+# index = 0
+
+# while index <= 100
+#   p index
+#   index += 5
+# end
+
 # 5. Write a while loop that prints the number 9000 ten times.
+
+# index = 0
+# while index <= 9
+#   p 9000
+#   index += 1
+# end
 
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
+# while true
+#   p "Please enter a number"
+#   input = gets.chomp
+#   if input.to_i > 10
+#     break
+#   end
+# end
+
 # 7. Write a while loop that prints the numbers 50 to 70.
+
+# number = 50
+# index = 0
+
+# while index < 20
+# end
 
 # 8. Write a while loop that prints the phrase "Around the world" 144 times.
 
+# index = 0
+
+# while index < 145
+#   p "Around the world"
+#   index += 1
+# end
+
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+# while true
+#   p "Please enter a word"
+#   input = gets.chomp
+#   if input.length >= 5
+#     break
+#   end
+# end
---
+++ strings_practice_problems.rb
@@ -0,0 +1,19 @@
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
