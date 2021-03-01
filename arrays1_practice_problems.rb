5cdf474ac54a19f81b0a370c747db092bdfef1be
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-01 10:19:55 -0500

    completes arrays1_practice

--- arrays1_practice_problems.rb
+++ arrays1_practice_problems.rb
@@ -1,23 +1,74 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
-words = ["one", "two", "three"]
-words.push("four", "five")
-p words
+# words = ["one", "two", "three"]
+# words.push("four", "five")
+# p words
 
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
-
+# letters = ["a", "b", "c", "d"]
+# letters[1] = 2
+# p letters
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+# index = 0
+# while index < numbers.length
+#   p numbers[index]
+#   index += 1
+# end
 
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 
+# number = [3]
+# number[1], number[2], number[3] = 5, 8, 100
+# p number
+
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+# letters = ["apple", "strawberry", "doughnut"]
+# letters[2] = letters[2].upcase
+# p letters
 
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 
+# index = 0
+# while index < names.length
+#   p names[index]
+#   index += 1
+# end
+
+# names = ["kim", "jim", "tim"]
+# names.each do |name|
+#   p name
+# end
+
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+# strings = ["happy", "and you know it"]
+# strings << "clap your hands"
+# p strings
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
+# numbers = [100, 3, 56, 72, 1] 
+# numbers[0] = numbers[0] * 10
+# p numbers
+
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
+# numbers = [5, 3]
+# index = 0 
+# while index < numbers.length
+#   p numbers[index]
+#   index += 1
+# end
+
+# numbers = [5, 3]
+# numbers.each do |number|
+#   p number
+# end
+
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+# countries = ["united states", "israel", "china"]
+# countries << "norway"
+# p countries
